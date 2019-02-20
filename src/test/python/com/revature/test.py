7ea7c8eee50e488411f36c129acd0de37f6c014f
Junjie Wei <junjiewei@localhost.localdomain>
2019-02-19 20:25:13 -0500

    finish logging

--- src/test/python/com/revature/test.py
+++ src/test/python/com/revature/test.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 
+import os
+
+
 '''
 This is your main testing script, this should call several other testing scripts on its own
 '''
+TESTS_EXECUTED = 0
+TEST_PASSES = 0
+
 def main():
-	print('TO-DO')
+	
+
+
+def Test(to_decorate_function):
+	def test_function(test)
+
+
+@Test
+def Authentication():
+	os.system(./main)
+	os.system("l")
+		 
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	main()
